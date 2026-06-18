148be8e52aeaee0b8a9678dea98861ebb677d480
valerisedu <connect@valerisedu.com>
2026-06-18 15:41:49 +0530

    Update firebase-config.js

--- portal/js/firebase-config.js
+++ portal/js/firebase-config.js
@@ -1,13 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/...";
-import { getFirestore } from "...";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  ...
+  apiKey: "AIzaSyCBjETh_8Wy2aqXJxW95xt-Waib2JqxTSU",
+  authDomain: "valeris-edu-portal.firebaseapp.com",
+  projectId: "valeris-edu-portal",
+  storageBucket: "valeris-edu-portal.firebasestorage.app",
+  messagingSenderId: "1085558997207",
+  appId: "1:1085558997207:web:22eae8cc6a91fece60f092",
+  measurementId: "G-7EJCQ925HW"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export { app };
+const analytics = getAnalytics(app);
