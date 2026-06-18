90402551f319e6d185a1aa120d29e1fb4ea2e4ba
valerisedu <connect@valerisedu.com>
2026-06-18 15:39:38 +0530

    Update firebase-config.js

--- portal/js/firebase-config.js
+++ portal/js/firebase-config.js
@@ -1,3 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/...";
+import { getFirestore } from "...";
+
 const firebaseConfig = {
-...
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  ...
 };
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
