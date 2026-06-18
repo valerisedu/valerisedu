57263b3732a1f3c1d68b1ddef8012696b6390d29
valerisedu <connect@valerisedu.com>
2026-06-18 15:52:40 +0530

    Update firebase-config.js

--- portal/js/firebase-config.js
+++ portal/js/firebase-config.js
@@ -1,21 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCBjETh_8Wy2aqXJxW95xt-Waib2JqxTSU",
   authDomain: "valeris-edu-portal.firebaseapp.com",
   projectId: "valeris-edu-portal",
   storageBucket: "valeris-edu-portal.firebasestorage.app",
   messagingSenderId: "1085558997207",
-  appId: "1:1085558997207:web:22eae8cc6a91fece60f092",
-  measurementId: "G-7EJCQ925HW"
+  appId: "1:1085558997207:web:22eae8cc6a91fece60f092"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
